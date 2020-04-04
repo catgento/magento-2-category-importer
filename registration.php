5dadba0c7b16803009e16404fac5839dafab6efc
Pau Iranzo <paugnu@gmail.com>
2020-04-04 13:51:57 +0200

    Fix incorrect registration

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Macopedia_CategoryImporter',
+    'Catgento_CategoryImporter',
     __DIR__
 );
